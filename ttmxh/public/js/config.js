6677f08f198acd601012062987edd8bcc98304a6
Nguyễn Ngọc Huy Tâm <108131520+anzing0983@users.noreply.github.com>
2023-03-06 12:40:26 +0700

    Add files via upload

---
+++ ttmxh/public/js/config.js
@@ -0,0 +1,59 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'Nguyen Ngoc Huy Tam',
+        'favicon' : 'https://i.pinimg.com/564x/99/fa/c3/99fac3b808ef5a79e891db1de9fcd7fa.jpg',
+        'notice': 'Nguyen Ngoc Huy Tam DTRAI S1'
+    },
+    'avatar-image' : 'https://i.pinimg.com/564x/99/fa/c3/99fac3b808ef5a79e891db1de9fcd7fa.jpg',
+    'name' : 'Nguyễn Ngọc Huy Tâm ', 
+    'is_tick' : true,
+    'skills' : [
+        'Student ',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'https://i.pinimg.com/564x/99/fa/c3/99fac3b808ef5a79e891db1de9fcd7fa.jpg',
+            'content' : 'CONTACT ADMIN',
+            'author' : 'Nguyễn Ngọc Huy Tâm',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Thông Tin Thanh Toán', 
+            'show-value' : 'Nguyễn Ngọc Huy Tâm',
+            'value' : 'https://nnht-bank.blogspot.com/',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@NguyenNgocHuyTam.Info' ,
+            'value' : 'https://www.facebook.com/NguyenNgocHuyTamxDtlacuti//',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'Zalo', 
+            'show-value' : 'Nguyễn Ngọc Huy Tâm',
+            'value' : 'https://zalo.me/0777522806',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'xxxx xxx xxx',
+            'value' : 'tel:xxxx xxx xxx',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'huytam2k7@gmail.com',
+            'value' : 'mailto:huytam2k7@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
